fdef6d7291aa7c46af213f7ab84c738a2ffb93d3
chanintorndti <chanintorn.dti@gmail.com>
2022-07-17 10:04:30 +0700

    SCV 4.0 : สร้าง model ไฟล์ diaryfood by nin

--- lib/views/message_ui.dart
+++ lib/views/message_ui.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class MessageUI extends StatelessWidget {
+class MessageUI extends StatefulWidget {
   const MessageUI({Key? key}) : super(key: key);
 
+  @override
+  State<MessageUI> createState() => _MessageUIState();
+}
+
+class _MessageUIState extends State<MessageUI> {
   @override
   Widget build(BuildContext context) {
     return Scaffold();
